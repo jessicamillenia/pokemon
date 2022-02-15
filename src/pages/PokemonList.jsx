2ff3c80409ea4f65ddc8298377cd04fb78d4bbff
jessicamillenia <jessicamillenia@gmail.com>
2022-02-15 23:51:54 +0700

    my list owned count

--- src/pages/PokemonList.jsx
+++ src/pages/PokemonList.jsx
@@ -1,6 +1,7 @@
 import { Grid, Card, CardActionArea, CardMedia, CardContent, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { RootContext } from "./Home";
 
 function PokemonList() {
   
@@ -38,36 +39,53 @@ function PokemonList() {
 
   if (!pokemons.length) return <h3 style={{textAlign: "center"}}>Loading...</h3>;
   return (
-    <div>
-      <Grid container
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            spacing={2}
-            padding={1}>
-        {pokemons.map((pokemon) => (
-          <Grid item key={pokemon.name} lg={2} md={3} sm={4} xs={6}>
-            <Card>
-              <CardActionArea component={Link} to={`/pokemon/${pokemon.name}`} >
-                <CardMedia
-                  component="img"
-                  image={pokemon.image}
-                />
-                <CardContent sx={{textAlign: 'center'}}>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {pokemon.name.toUpperCase()}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Owned Total : 10
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-  );
+    <RootContext.Consumer>
+      {
+        value => {
+          function countpokemon(name){
+            let count = 0;
+            value.state.myPokemon.forEach((mypoke) => {
+              if (mypoke.name === name){
+                count += 1;
+              } 
+            })
+            return (<span>{count}</span>)
+          }
+          return (
+            <div>
+              <Grid container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={2}
+                    padding={1}>
+                {pokemons.map((pokemon) => (
+                  <Grid item key={pokemon.name} lg={2} md={3} sm={4} xs={6}>
+                    <Card>
+                      <CardActionArea component={Link} to={`/pokemon/${pokemon.name}`} >
+                        <CardMedia
+                          component="img"
+                          image={pokemon.image}
+                        />
+                        <CardContent sx={{textAlign: 'center'}}>
+                          <Typography gutterBottom variant="h6" component="div">
+                            {pokemon.name.toUpperCase()}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            Owned Total : {countpokemon(pokemon.name)}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
+          );
+        }
+      }
+    </RootContext.Consumer>
+  )
 }
 
 export default PokemonList;
